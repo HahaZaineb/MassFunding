60605e1e73faa1836f1ba290aedede9b1782170f
ZainebHaha <hahazaineb21@gmail.com>
2025-04-10 00:00:12 +0200

    .

--- my-massa-dapp/vite.config.ts
+++ my-massa-dapp/vite.config.ts
@@ -4,22 +4,13 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Disable TypeScript checking in development
-      babel: {
-        plugins: [["@babel/plugin-transform-typescript", { allowDeclareFields: true }]],
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    // Disable TypeScript checking during build
-    sourcemap: true,
     // Ensure we're building for the web
     target: "esnext",
     // Output to the dist folder
