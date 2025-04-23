16df305c3e56ba30bdf3bcd8ffc48dfbcf085c4e
ZainebHaha <hahazaineb21@gmail.com>
2025-04-23 11:19:29 +0200

    fixed dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import React from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import RequestFunding from "../components/request-funding"
 import Fund from "../components/fund"
