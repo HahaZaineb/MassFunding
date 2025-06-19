9d632749b1f0dd4c7236c82b800ae2ce37f930cd
ZainebHaha <hahazaineb21@gmail.com>
2025-06-19 11:46:31 +0100

    contrat

--- my-massa-dapp/src/configs/massa.ts
+++ my-massa-dapp/src/configs/massa.ts
@@ -1,3 +1,3 @@
-export const CONTRACT_ADDRESS = "AS1seJRkP6SSxf49qRyfi4zLnknBnTtEZ7iGRAMvBupPidxHsTSn"
+export const CONTRACT_ADDRESS = "AS1taPqa3rfZg9o33Lfgi7jcWPhC8L2woJw3TyHXLCBBDgesqo11"
 
 export const MASSA_NETWORK: 'buildnet' | 'mainnet' = 'buildnet'
